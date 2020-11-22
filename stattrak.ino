feddd1072ba1278f474ba55cedb15fa31f31673c
spockly <spockly@gmail.com>
2020-11-22 17:59:55 -0500

    Update stattrak.ino

--- stattrak.ino
+++ stattrak.ino
@@ -8,11 +8,11 @@ LedControl lc=LedControl(5,4,0,1); // lc is our object
 // pin 11 is connected to the CLK pin 13
 // pin 10 is connected to LOAD pin 12
 // 1 as we are only using 1 MAX7219
-const char* ssid = "router";
-const char* password = "password";
+const char* ssid = "{ssid}";
+const char* password = "{pw}";
 
 //Your Domain name with URL path or IP address with path
-String serverName = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&steamid=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+String serverName = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key={key}&steamid={steam}";
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
@@ -36,10 +36,11 @@ void setup()
     delay(500);
     Serial.print(".");
   }
-}
-void loop()
-{
-    for (int b=0; b<9; b++)
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  Serial.print("The IP Address of ESP8266 Module is: ");
+  Serial.print(WiFi.localIP());// Print the IP address
+  for (int b=0; b<10; b++)
        {
     for (int a=0; a<6; a++)
       {
@@ -48,21 +49,31 @@ void loop()
     }
       delay(250);
     }
+    for (int b=0; b<10; b++)
+       {
+    for (int a=0; a<6; a++)
+      {
+    lc.setDigit(0,a,b,false);
 
+    }
+      delay(250);
+    }
+}
+void loop()
+{
   if ((millis() - lastTime) > timerDelay) {
-    if(WiFi.status()== WL_CONNECTED){
+    if (WiFi.status()== WL_CONNECTED){
       HTTPClient http;
-      String serverPath = serverName;
-      http.begin(serverPath.c_str());
-      
+      http.begin(serverName.c_str());
       // Send HTTP GET request
       int httpResponseCode = http.GET();
       String payload = http.getString();
       const char* conststring = payload.c_str();
+      Serial.println(httpResponseCode);
       if (httpResponseCode>0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
-        const size_t capacity = JSON_ARRAY_SIZE(164) + JSON_ARRAY_SIZE(212) + JSON_OBJECT_SIZE(1) + 376*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + 8620;
+        const size_t capacity = JSON_ARRAY_SIZE(85) + JSON_ARRAY_SIZE(170) + JSON_OBJECT_SIZE(1) + 255*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + 10150;
         DynamicJsonDocument doc(capacity);
         DeserializationError err = deserializeJson(doc, conststring);
         if (err) {
@@ -71,14 +82,32 @@ void loop()
           return;
         }
         JsonObject playerstats = doc["playerstats"];
+        JsonArray playerstats_stats = playerstats["stats"];
         const char* playerstats_steamID = playerstats["steamID"];
-        Serial.println(playerstats_steamID);      
-        }
+        int kills = playerstats_stats[0]["value"];
+        //Serial.println(payload);
+        //Serial.println("Help");
+         Serial.println((kills%10));
+         Serial.println((kills%100)/10);
+         Serial.println((kills%1000)/100);
+         Serial.println((kills%10000)/1000);
+         Serial.println((kills%100000)/10000);
+         lc.setDigit(0,0,((kills%1000000)/100000),false);
+         lc.setDigit(0,1,((kills%100000)/10000),false);
+         lc.setDigit(0,2,((kills%10000)/1000),false);
+         lc.setDigit(0,3,((kills%1000)/100),false);
+         lc.setDigit(0,4,((kills%100)/10),false);
+         lc.setDigit(0,7,((kills%10)),false);
+
+        
+                              delay(600000);
       http.end();
-    }
+      } 
     }
     else {
       Serial.println("WiFi Disconnected");
-    } 
+    }
+     
     lastTime = millis();
+  }
   }
